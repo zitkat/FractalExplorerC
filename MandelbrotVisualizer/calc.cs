42294c6df9ef89849eca970f130d466a0d5c39a5
zitkat <zitkat@students.zcu.cz>
2015-05-23 18:09:45 +0200

    ColorF.cs a calc.cs

---
+++ MandelbrotVisualizer/calc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace FractalExplorer
+{
+    class calc
+    {
+        /*
+         * expression
+         */
+
+
+        public Complex nextIteration(Complex z, Complex c)
+        {
+            //TODO calculate from expression
+            double re = z.Real*z.Real - z.Imaginary * z.Imaginary + c.Real;
+            double im = 2 * z.Imaginary * z.Real + c.Imaginary;
+            Complex res = new Complex(re, im);
+            return res;
+        }
+
+        public void setExpression(String s)
+        {
+
+        }
+
+        private void/*expression*/ parseExp(String s)
+        {
+
+        }
+
+        public static double fsq(Complex z)
+        {
+            return z.Real * z.Real + z.Imaginary * z.Imaginary;
+        }
+
+
+
+
+    }
+}
